42f5ae55024c789998b7429f7d19c590bd938553
abishek-karnan <pabishek@student.tce.edu>
2024-08-27 20:08:09 +0530

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,83 @@
+const INFO = {
+	main: {
+		title: "ABISHEK P",
+		name: "ABISHEK PALANISAMY",
+		email: "pabishek@student.tce.edu",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "I am Abishek. I am a web Designer, developer and Problem Solver ",
+		description:
+			"As an IT Engineer, I possess proficient problem-solving, leadership, and management skills with strong interpersonal skills and decision-making abilities with team adaptability..",
+	},
+
+	about: {
+		title: "I’m Abishek P I live in Madurai, From  where I design the future",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "SMART ROADWAYS TOOLING SYSTSEM ",
+			description:
+				"Developed a mobile application to modernize toll collection with features like location tracking, multimedia support, emergency calling, and bill generation system.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "VISUAL ASSISTANCE SYSTEM",
+			description:
+				"Developed a system to assist visually impaired individuals by transforming the visual world into      audio signals. The project focuses on real-time object detection and position estimation, informing users about objects and their spatial locations to enhance their understanding of the environment and improve safety.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "PARKING MANAGEMENT SYSTEM(CONSOLE APPLICATION",
+			description:
+				"Implemented OOP Principles: Applied object-oriented programming concepts to design a modular and scalable Parking Management System using Java.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{title: "SMART ROADWAYS TOOLING SYSTSEM ",
+			description:
+				"Developed a mobile application to modernize toll collection with features like location tracking, multimedia support, emergency calling, and bill generation system.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "PARKING MANAGEMENT SYSTEM(CONSOLE APPLICATION",
+			description:
+				"Implemented OOP Principles: Applied object-oriented programming concepts to design a modular and scalable Parking Management System using Java.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
